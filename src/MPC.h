66995cd3c446ebb2ba2b543bb7eb277e4973183e
vikasmalik22 <vikasmalik22@gmail.com>
2018-09-09 00:33:31 +0800

    Update MPC.h

--- src/MPC.h
+++ src/MPC.h
@@ -13,8 +13,8 @@ class MPC {
   virtual ~MPC();
 
   // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs);
+  // Return the first actuations.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
